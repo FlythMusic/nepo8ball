780b258f7ec0e4b1c4d6036897bc91c1d180bbae
FlythMusic <47484864+FlythMusic@users.noreply.github.com>
2019-03-16 22:25:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,6 +66,6 @@ bot.on("message", (message) => {
                 console.log(o_O);
                 message.channel.send({ embed: embedError })
             });
-        bot.user.setActivity('Rexor's Discord')
+        bot.user.setActivity('Discord')
     }
 });
